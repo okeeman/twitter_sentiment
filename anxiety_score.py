1b994432bfc7b38ff3b699f8dc493b47cde1b86e
okeeman <owen.sweeney@ittd.ie>
2026-08-20 19:43:31 +0100

    Delete anxiety_score.py

--- anxiety_score.py
+++
@@ -1,6 +0,0 @@
-"""
-This is a feature created by determining a list of words which would indicate anxiety.
-For each word in the tweet which has an anxiety word add one to the anxiety score.
-
-This mightn't work as it would possibly depend on context.
-"""
